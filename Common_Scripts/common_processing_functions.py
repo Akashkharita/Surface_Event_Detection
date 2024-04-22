f4cd43f5c665e43ab5b6ae71d4a1400424ca5123
Akash Kharita <akharita1999@gmail.com>
2024-04-22 16:52:55 -0700

    Adding Files

---
+++ Common_Scripts/common_processing_functions.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import h5py
+import obspy
+from obspy.signal.filter import envelope
+from obspy.clients.fdsn import Client
+from tqdm import tqdm
+from glob import glob
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, auc, classification_report, confusion_matrix, f1_score
+from sklearn.utils.multiclass import unique_labels
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+#from xgboost import XGBClassifier
+from sklearn.decomposition import PCA
+from imblearn.under_sampling import RandomUnderSampler
+from scipy import stats, signal
+from sklearn.datasets import load_iris
+from sklearn.metrics import precision_score, recall_score
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+pd.set_option('display.max_columns', None)
+from obspy.geodetics.base import gps2dist_azimuth
+from datetime import datetime, timedelta
+import time
+#import lightgbm as lgb
+import re
+import tsfel
+import random
+import calendar
+import concurrent.futures
+
+
+# We need to change the path to import the seis feature library. 
+
+import sys
+sys.path.append('../Feature_Extraction_Scripts/Physical_Feature_Extraction_Scripts/')
+import seis_feature
+from seis_feature import compute_physical_features
+from multiprocessing import Pool
+from tsfel import time_series_features_extractor, get_features_by_domain
+from functools import partial
+import multiprocessing
+
+
+
+
+def apply_cosine_taper(arrays, taper_percent=10):
+    tapered_arrays = []
+    
+    #print(arrays.shape)
+    num_samples = arrays.shape[1]  # Assuming each sub-array has the same length
+    
+    for array in arrays:
+        
+
+        taper_length = int(num_samples * taper_percent / 100)
+        taper_window = np.hanning(2 * taper_length)
+        
+     
+        tapered_array = array.copy()
+        tapered_array[:taper_length] = tapered_array[:taper_length] * taper_window[:taper_length]
+        tapered_array[-taper_length:] = tapered_array[-taper_length:] * taper_window[taper_length:]
+        
+        tapered_arrays.append(tapered_array)
+    
+    return np.array(tapered_arrays)              
+              
+    
+    
+
+def butterworth_filter(arrays, lowcut = 1, highcut = 10, fs = 100, num_corners = 4, filter_type='bandpass'):
+    """
+    Apply a Butterworth filter (bandpass, highpass, or lowpass) to each array in an array of arrays.
+
+    Parameters:
+        arrays (list of numpy arrays): List of arrays to be filtered.
+        lowcut (float): Lower cutoff frequency in Hz.
+        highcut (float): Upper cutoff frequency in Hz.
+        fs (float): Sampling frequency in Hz.
+        num_corners (int): Number of corners (filter order).
+        filter_type (str, optional): Type of filter ('bandpass', 'highpass', or 'lowpass'). Default is 'bandpass'.
+
+    Returns:
+        list of numpy arrays: List of filtered arrays.
+    """
+    filtered_arrays = []
+    for data in arrays:
+        # Normalize the frequency values to Nyquist frequency (0.5*fs)
+        lowcut_norm = lowcut / (0.5 * fs)
+        highcut_norm = highcut / (0.5 * fs)
+
+        # Design the Butterworth filter based on the filter type
+        if filter_type == 'bandpass':
+            b, a = signal.butter(num_corners, [lowcut_norm, highcut_norm], btype='band')
+        elif filter_type == 'highpass':
+            b, a = signal.butter(num_corners, lowcut_norm, btype='high')
+        elif filter_type == 'lowpass':
+            b, a = signal.butter(num_corners, highcut_norm, btype='low')
+        else:
+            raise ValueError("Invalid filter_type. Use 'bandpass', 'highpass', or 'lowpass'.")
+
+        # Apply the filter to the data using lfilter
+        filtered_data = signal.lfilter(b, a, data)
+
+        filtered_arrays.append(filtered_data)
+
+    return filtered_arrays
+
+              
+    
+    
+    
